f2023f2e03536bea28dada14250e4a881b80a6ef
Mohsan1995 <mohsan.dev@gmail.com>
2016-06-14 15:11:04 +0200

    add navigation view

--- retrofit/app/src/main/java/fr/esgi/retrofit/MainActivity.java
+++ retrofit/app/src/main/java/fr/esgi/retrofit/MainActivity.java
@@ -1,31 +1,124 @@
 package fr.esgi.retrofit;
 
-import android.content.SharedPreferences;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
-/**
- * Created by mohsan on 14/06/16.
- */
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    DrawerLayout drawer;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    FloatingActionButton fab;
+
+    ActionBarDrawerToggle toggle;
+    FragmentManager fm;
 
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-
-
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        drawer= (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView= (NavigationView) findViewById(R.id.nav_view);
+        fab= (FloatingActionButton) findViewById(R.id.fab);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        //Permet d'ajouter les bars pour ouvrir la navigation view
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+//        navigationView.setNavigationItemSelectedListener(this);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
